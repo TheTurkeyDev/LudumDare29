f62b659ef6236ffe2deb821a596448b4e4b71d85
turkey2349 <Turkey2349@gmail.com>
2014-04-27 16:54:31 -0400

    Chnaged to mouse listener

--- src/com/turkey/LD29/listeners/ScreenMouseListener.java
+++ src/com/turkey/LD29/listeners/ScreenMouseListener.java
@@ -1,13 +1,13 @@
 package com.turkey.LD29.listeners;
 
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import com.turkey.LD29.screen.ScreenManager;
 import com.turkey.LD29.screen.util.Interactable;
 import com.turkey.LD29.util.Location;
 
-public class ScreenMouseListener extends MouseAdapter
+public class ScreenMouseListener implements MouseListener
 {
 	int x = 0, y = 0;
 	public void mouseClicked(MouseEvent e)
@@ -25,21 +25,30 @@ public class ScreenMouseListener extends MouseAdapter
 				ScreenManager.getInstance().getCurrentScreen().onClicked(i);
 			}
 		}
-		/*for(Interactable i: ScreenManager.getInstance().getCurrentScreen().getInteractables())
-		{
-			if(i.contains(x, y))
-			{
-				if(i.isClick())
-				{
-					i.setSelected(!i.isSelected());
-				}
-				ScreenManager.getInstance().getCurrentScreen().onClicked(i);
-			}
-		}*/
 	}
 	
 	public Location getLastClick()
 	{
 		return new Location(x ,y);
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0)
+	{
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0)
+	{
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0)
+	{
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0)
+	{	
 	}
 }
